eee4871cceee8ad0b4414b5b6ffcbd7466663b0d
Gabriel Padron <gabrielpadc@gmail.com>
2021-09-08 16:13:37 -0400

    fix restart and terminal colors

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
+extern crate colored;
 extern crate text_io;
-use text_io::{read, try_read};
+use colored::*;
+use text_io::read;
 
 type Board = Vec<char>;
 
 fn main() {
-    println!("Bem-vindo ao jogo da velha");
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    println!("\n{}", " ##### Bem-vindo ao jogo da velha #####".green());
     game();
-
-    println!("Você deseja jogar novamente? (s/n)");
-    let restart: char = read!();
-    if restart == 's' || restart == 'S' {
-        game();
-    }
 }
 
 fn print_board(board: &mut Board) {
     println!("");
-    println!("{} | {} | {}", board[0], board[1], board[2]);
-    println!("--+---+--");
-    println!("{} | {} | {}", board[3], board[4], board[5]);
-    println!("--+---+--");
-    println!("{} | {} | {}", board[6], board[7], board[8]);
+    println!("\t{} | {} | {}", board[0], board[1], board[2]);
+    println!("\t--+---+--");
+    println!("\t{} | {} | {}", board[3], board[4], board[5]);
+    println!("\t--+---+--");
+    println!("\t{} | {} | {}", board[6], board[7], board[8]);
     println!("");
 }
 
@@ -37,25 +31,33 @@ fn game() {
     let mut count: i32 = 0;
     let mut finished: bool = false;
 
-    println!("Insira o nome do primeiro jogador: ");
+    println!("{}", "Insira o nome do primeiro jogador: ".yellow());
     let player1: String = read!();
 
-    println!("Insira o nome do segundo jogador: ");
+    println!("{}", "Insira o nome do segundo jogador: ".yellow());
     let player2: String = read!();
     let mut player_turn = &player1;
 
     while !finished {
         print_board(&mut board);
 
-        println!("{} ({}), insira um inteiro entre 1 e 9:", player_turn, turn);
-        let mut choice: usize = try_read!().unwrap();
+        println!(
+            "{} ({}){}",
+            player_turn.green(),
+            turn,
+            ", insira um inteiro entre 1 e 9:".green()
+        );
+        let mut choice: usize = read!();
         choice -= 1;
 
         if is_empty_cell(&board, choice) {
             board[choice] = turn;
             count += 1;
         } else {
-            println!("\nEste campo já está ocupado, selecione outro campo:");
+            println!(
+                "\n{}",
+                "Este campo já está ocupado, selecione outro campo:".red()
+            );
             continue;
         }
 
@@ -63,45 +65,85 @@ fn game() {
             // horizontal
             if board[6] == board[7] && board[7] == board[8] && !is_empty_cell(&board, 8) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             } else if board[3] == board[4] && board[4] == board[5] && !is_empty_cell(&board, 5) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             } else if board[0] == board[1] && board[1] == board[2] && !is_empty_cell(&board, 2) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
 
                 // vertical
             } else if board[0] == board[3] && board[3] == board[6] && !is_empty_cell(&board, 6) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             } else if board[1] == board[4] && board[4] == board[7] && !is_empty_cell(&board, 7) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             } else if board[2] == board[5] && board[5] == board[8] && !is_empty_cell(&board, 8) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
 
                 // diagonal
             } else if board[0] == board[4] && board[4] == board[8] && !is_empty_cell(&board, 8) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             } else if board[2] == board[4] && board[4] == board[6] && !is_empty_cell(&board, 6) {
                 print_board(&mut board);
-                println!(" ###### Fim de jogo, {} ganhou! ######", player_turn);
+                println!(
+                    "{} {} {}",
+                    " ###### Fim de jogo,".yellow(),
+                    player_turn.yellow(),
+                    "ganhou! ######".yellow()
+                );
                 finished = true;
             }
         }
 
         if count == 9 {
-            println!(" ###### Fim de jogo, foi um empate! ######");
+            println!("{}", " ###### Fim de jogo, foi um empate! #####".blue(),);
             finished = true;
         }
 
@@ -112,5 +154,12 @@ fn game() {
             turn = 'X';
             player_turn = &player1;
         }
+    }
+
+    println!("Você deseja jogar novamente? (s/n)");
+    let restart: char = read!();
+    if restart == 's' || restart == 'S' {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        game();
     }
 }
