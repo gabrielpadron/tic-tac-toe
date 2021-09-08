998a1dd1e2e0e42558cfd7efeb68302bde617cc0
Gabriel Padron <gabrielpadc@gmail.com>
2021-09-08 09:40:16 -0400

    hashmap to vec and match board points

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,60 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
 use std::io;
+type Board = Vec<Vec<char>>;
 
 fn main() {
     println!("Bem-vindo ao jogo da velha");
     game();
 }
 
-fn print_board(board: &mut HashMap<i32, &str>) {
+fn print_board(board: Board) {
     println!("");
-    println!("{} | {} | {}", board[&1], board[&2], board[&3]);
+    println!("{} | {} | {}", board[0][0], board[0][1], board[0][2]);
     println!("--+---+--");
-    println!("{} | {} | {}", board[&4], board[&5], board[&6]);
+    println!("{} | {} | {}", board[1][0], board[1][1], board[1][2]);
     println!("--+---+--");
-    println!("{} | {} | {}", board[&7], board[&8], board[&9]);
+    println!("{} | {} | {}", board[2][0], board[2][1], board[2][2]);
     println!("");
-}
-
-fn is_empty() -> bool {
-    true
 }
 
 fn game() {
-    let mut board = HashMap::new();
-    board.insert(1, "1");
-    board.insert(2, "2");
-    board.insert(3, "3");
-    board.insert(4, "4");
-    board.insert(5, "5");
-    board.insert(6, "6");
-    board.insert(7, "7");
-    board.insert(8, "8");
-    board.insert(9, "9");
+    let mut board: Board = vec![
+        vec!['1', '2', '3'],
+        vec!['4', '5', '6'],
+        vec!['7', '8', '9'],
+    ];
 
-    let turn = 'X';
-    let mut _count: i32;
+    let turn = "X";
+    let mut count: i32 = 0;
     let finished = false;
 
     while !finished {
-        print_board(&mut board);
+        print_board(board.clone());
         println!("Insira um inteiro entre 1 e 9:");
 
         let mut line = String::new();
-        io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read stdin");
-        let choice = line.trim().parse::<i32>().expect("Failed to parse input");
+        io::stdin().read_line(&mut line).unwrap();
+        let choice = line.trim().parse::<usize>().unwrap();
+        let (row, col) = match choice {
+            1 => (0, 0),
+            2 => (0, 1),
+            3 => (0, 2),
+            4 => (1, 0),
+            5 => (1, 1),
+            6 => (1, 2),
+            7 => (2, 0),
+            8 => (2, 1),
+            _ => (2, 2),
+        };
 
-        if board[&choice].is_empty() {
-
+        if board[row][col] != 'X' && board[row][col] != 'O' {
+            board[row][col] = 'X';
+            count += 1;
+        } else {
+            println!("Este campo já está ocupado, \nSelecione outro campo:");
+            continue;
         }
     }
-
 }
